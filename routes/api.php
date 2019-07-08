0206554f49b8a19878c0edf95312c67fc0fd2f5f
qianyewoailuo <qianyewoailuo@126.com>
2019-07-08 10:45:52 +0000

    first api for select version

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,22 @@ use Illuminate\Http\Request;
 |
 */
 
+// 示例 api
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// 第一个 dingo/api 测试 - version测试选择
+$api = app(Dingo\Api\Routing\Router::class);
+
+$api->version('v1',function($api){
+    $api->get('version',function(){
+        return response('this is version v1 response');
+    });
+});
+
+$api->version('v2',function($api){
+    $api->get('version',function(){
+        return response('this is version v2 response');
+    });
 });
